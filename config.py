7f8ecf69be5542eea5c377adfa37fe8b77c8298d
Kevin <kevinambundo3@gmail.com>
2020-10-26 17:27:07 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+
+class Config:
+
+    SECRET_KEY="mykey"
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kevin:Password2@localhost/pitches'
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kevin:Password2@localhost/pitches'    
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kevin:Password2@localhost/pitches_test'    
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,3 @@
+export SECRET_KEY='mykey'
+export MAIL_USERNAME='kevinambundo404@gmail.com'
+export MAIL_PASSWORD='KevinAmbundo404.'
